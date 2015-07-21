d01d60bc48ba05ce73920de7e4e64aa8f3a75c81
John Spencer <johnspencer15@gmail.com>
2015-07-21 13:55:24 -0700

    Add down arrow and scroll effect

--- app/src/js/app.js
+++ app/src/js/app.js
@@ -1,5 +1,9 @@
 var main = function() {
-
+	$('.arrow-down').click(function() {
+		$('body').animate({
+			scrollTop: $('#about').offset().top - 60	
+		}, 1000);
+	});
 };
 
 $(document).ready(main());
--- app/src/js/production.js
+++ app/src/js/production.js
@@ -39940,7 +39940,11 @@ if (typeof jQuery === 'undefined') {
 }(jQuery);
 
 var main = function() {
-
+	$('.arrow-down').click(function() {
+		$('body').animate({
+			scrollTop: $('#about').offset().top - 60	
+		}, 1000);
+	});
 };
 
 $(document).ready(main());
